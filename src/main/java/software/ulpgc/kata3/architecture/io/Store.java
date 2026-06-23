5f2d4b043df02bb0dc6c96d473994fd0720b0701
ivanperezdiaz829 <ivanperezdiaz829@gmail.com>
2026-06-23 18:03:51 +0100

    Kata 4 completed

--- src/main/java/software/ulpgc/kata3/architecture/io/Store.java
+++ src/main/java/software/ulpgc/kata3/architecture/io/Store.java
@@ -3,7 +3,6 @@ package software.ulpgc.kata3.architecture.io;
 import software.ulpgc.kata3.architecture.model.Movie;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.stream.Stream;
 
 public interface Store {
